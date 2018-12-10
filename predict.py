de8fb7498e4cc967fa9a12181d280dc5f1e36a09
Chris Nguyen <cnguyen0320@gmail.com>
2018-12-10 12:11:26 -0600

    reduce overhead in predict.py

--- predict.py
+++ predict.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import NN
-import train
-import math, random, json, sys
+import json, sys
 
 def loadWeights(filename):
     """Loads the saved weights and creates lists with them"""
@@ -25,15 +24,12 @@ def loadJSON(filename):
     parameters = json.load(data)
     data.close()
     
-    training_file = parameters['data']['training_data']
     numAttributes = parameters['data']['num_attributes']
     numDigits = parameters['data']['num_classes']
     numHiddenNodes = parameters['network']['num_hidden_nodes']
-    learning_rate = parameters['network']['learning_rate']
-    maxEpoch = parameters['network']['max_epochs']
     seed = parameters['network']['seed']
     
-    return training_file, numAttributes, numDigits,numHiddenNodes,learning_rate,maxEpoch,seed
+    return numAttributes,numDigits,numHiddenNodes, seed
 
 def test(network, trainingSet):
     count = 0
@@ -51,23 +47,23 @@ if __name__ == '__main__':
         outputfile = str(sys.argv[3])
         hiddenWeights = loadWeights(hiddenfile)
         outputWeights = loadWeights(outputfile)
-        training_file, numAttributes, numDigits,numHiddenNodes,learning_rate,maxEpoch,seed = loadJSON(json_file)
+        numAttributes, numDigits,numHiddenNodes,seed = loadJSON(json_file)
         
     except:
-        print('Error loading. usage: python train.py <parameter_file> <hiddenweights_file> <outputweights_file' )
+        print('Error loading. usage: python train.py <parameter_file> <hiddenweights_file> <outputweights_file>' )
         exit(1)
     
     
     #Creates a network using the parameters set in train.py and the loaded hidden and output weights
-    network = NN.Network(None, None, numAttributes, numDigits, numHiddenNodes, learning_rate, maxEpoch, hiddenWeights, outputWeights)
+    network = NN.Network(None, None, numAttributes, numDigits, numHiddenNodes, None, None, hiddenWeights, outputWeights)
     
 
-    inst_list = train.parseDataSet(training_file, numAttributes)    
-    random_index = math.floor(random.random()*len(inst_list))    
-    inst = inst_list[random_index]
-    
-    print("Known value:", inst.value)
-    
-    print("The detected digit is a", network.predict(inst))
-    
-    print("success rate: ", test(network, inst_list))
+#    inst_list = train.parseDataSet(training_file, numAttributes)    
+#    random_index = math.floor(random.random()*len(inst_list))    
+#    inst = inst_list[random_index]
+#    
+#    print("Known value:", inst.value)
+#    
+#    print("The detected digit is a", network.predict(inst))
+#    
+#    print("success rate: ", test(network, inst_list))
